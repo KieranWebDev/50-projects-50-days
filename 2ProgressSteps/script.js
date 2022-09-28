ed26bc3fd17856b48d496af5e5597bf4d7092047
Kieran <kieran.webdevelopment@gmail.com>
2022-09-28 11:47:36 +0100

    project 2. js completed

--- 2ProgressSteps/script.js
+++ 2ProgressSteps/script.js
@@ -2,3 +2,49 @@ const progress = document.getElementById('progress');
 const btnPrev = document.getElementById('prev');
 const btnNext = document.getElementById('next');
 const circles = document.querySelectorAll('.circle');
+
+let currentActive = 1;
+
+btnNext.addEventListener('click', function () {
+  currentActive++;
+
+  if (currentActive >= circles.length) {
+    currentActive = circles.length;
+  }
+  update();
+  console.log(currentActive);
+});
+
+btnPrev.addEventListener('click', function () {
+  currentActive--;
+
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+  update();
+  console.log(currentActive);
+});
+
+function update() {
+  circles.forEach((circle, index) => {
+    if (index < currentActive) {
+      circle.classList.add('active');
+    } else {
+      circle.classList.remove('active');
+    }
+  });
+
+  const actives = document.querySelectorAll('.active');
+
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + '%';
+
+  if (currentActive === 1) {
+    btnPrev.disabled = true;
+  } else if (currentActive === circles.length) {
+    btnNext.disabled = true;
+  } else {
+    btnPrev.disabled = false;
+    btnNext.disabled = false;
+  }
+}
