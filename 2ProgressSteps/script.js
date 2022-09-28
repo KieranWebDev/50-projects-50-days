8e699e523c134366bbf57b91877534cc75136444
Kieran <kieran.webdevelopment@gmail.com>
2022-09-28 11:02:02 +0100

    project 2. html and css done

---
+++ 2ProgressSteps/script.js
@@ -0,0 +1,4 @@
+const progress = document.getElementById('progress');
+const btnPrev = document.getElementById('prev');
+const btnNext = document.getElementById('next');
+const circles = document.querySelectorAll('.circle');
